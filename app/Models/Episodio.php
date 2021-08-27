3d7443321caf09cee955abaa98953fa2424ef7bc
Déborah Fonseca <deborahferreirafonseca@gmail.com>
2021-08-27 07:15:38 -0300

    Criando models
    Criando model para Série e Episódios

---
+++ app/Models/Episodio.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace App\Models;
+
+use \Illuminate\Database\Eloquent\Model;
+
+/**
+ * Description of Episodio
+ *
+ * @author deborah
+ */
+class Episodio extends Model
+{
+    public $timestamps = false;
+    protected $fillable = ['temporada', 'numero', 'assistido'];
+    
+    public function serie() 
+    {
+        return $this->belongsTo(Serie::class);
+    }
+}
---
+++ app/Models/Serie.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace App\Models;
+
+use \Illuminate\Database\Eloquent\Model;
+
+/**
+ * Description of Serie
+ *
+ * @author deborah
+ */
+class Serie extends Model 
+{
+    public $timestamps = false;
+    protected $fillable = ['nome'];
+    
+    public function episodios()
+    {
+        return $this->hasMany(Episodio::class);
+        
+    }
+
+}
